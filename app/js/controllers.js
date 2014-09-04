740557f33c43e0265abbf518226eabbfbab8e0d6
Mohammed Arif <arif_iq@yahoo.co.in>
2014-09-04 22:13:42 +0530

    Testing the directive scope.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,8 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+	.controller('AppCtrl',
+		function($scope) {
+			$scope.ctrlFlavor = "blackberry";
+		}
+);
--- app/js/directives.js
+++ app/js/directives.js
@@ -4,8 +4,22 @@
 
 
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+directive('appVersion', ['version',
+	function(version) {
+		return function(scope, elm, attrs) {
+			elm.text(version);
+		};
+	}
+])
+
+.directive('drink', function(){
+	return {
+		scope: {
+			flavor: "@"
+		},
+		template: '<div>{{flavor}}</div>'
+		// link: function(scope, elem, attrs) {
+		// scope.flavor = attrs.flavor;
+		// }
+	};
+});
